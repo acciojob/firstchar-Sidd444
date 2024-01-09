c5038fa60cf22034720f337d0786ce5cc8983f47
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2024-01-09 19:48:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+  if(text.length===0) return "";
+   let f="";
+	for(let i=0;i<text.length;i++){
+		if(f==="" && text[i]!==" ") f=text[i];
+	}
+	return f;
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
-alert(firstChar(text));
+alert(firstChar(text));   
+ 
